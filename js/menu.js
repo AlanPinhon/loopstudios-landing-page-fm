9b85ad27e4e86de02b4ab6aaeff6d82f92d40cf0
Alan <pinhoneibor32@gmail.com>
2023-07-14 23:43:37 -0600

    hamburger menu finished

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('.menu-icon');
+
+const handleMenu = () => {
+  const headerNav = document.querySelector('.nav-header');
+
+  if(!headerNav.classList.contains('show')){
+    document.body.classList.add('no-scroll');
+    headerNav.classList.add('show');
+    menu.setAttribute('src','../images/icon-close.svg');
+  } else {
+    document.body.classList.remove('no-scroll');
+    headerNav.classList.remove('show');
+    menu.setAttribute('src','../images/icon-hamburger.svg');
+  }
+}
+
+
+menu.addEventListener('click', handleMenu)
